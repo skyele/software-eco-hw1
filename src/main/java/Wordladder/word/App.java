fd6e08def6adafab672f6e6af4b5cc3e7c94d359
skyele <skyele@sjtu.edu.cn>
2018-03-10 18:04:13 +0800

    finish code part

--- src/main/java/Wordladder/word/App.java
+++ src/main/java/Wordladder/word/App.java
@@ -7,8 +7,10 @@ import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
- * Hello world!
- *
+ * 作者： 徐天强
+ * 学号： 516030910391
+ * 日期： 2018.3.10
+ * 功能： 实现一个wordladder 
  */
 public class App 
 {
@@ -16,78 +18,84 @@ public class App
 	
     public static void main( String[] args )
     {
-    	HashSet<String>words = read_words();	//read words in the dictionary
+    		HashSet<String>words = read_words();	//read words in the dictionary
     	
-    	String word1 = "", word2 = "";
-    	int looptime = 0;
-    	char ch = ' ';        //used for checking '\n' ,if user press "enter",then the program ends
-    	while (true) {
-    		if (looptime > 0) {
+    		String word1 = "", word2 = "";
+    		int looptime = 0;
+    		
+    		while (true) {
     			System.out.println();   //add a empty line after one inquiry
-    		}
-    		System.out.println("Word #1 (or Enter to quit): ");
-    		Scanner scan = new Scanner(System.in);
+    			System.out.println("Word #1 (or q for quit): ");
+    			Scanner scan = new Scanner(System.in);
     		
-    		if(!scan.hasNext())
-    		{
-    			break;
-    		}
-    		word1 = scan.next();
-    		scan.close();
-    		System.out.println("Word #2 (or Enter to quit): ");
-    		scan = new Scanner(System.in);
+    			if(!scan.hasNext())
+    			{
+    				break;
+    			}
+    			word1 = scan.next();
+    			if(word1.equals("q"))
+    			{
+    				break;
+    			}
+    			System.out.println("Word #2 (or Enter to quit): ");
+    			scan = new Scanner(System.in);
     		
-    		if(!scan.hasNext())
-    		{
-    			break;
-    		}
+    			if(!scan.hasNext())
+    			{
+    				break;
+    			}
     		
-    		word2 = scan.next();
-    		word1 = word1.toLowerCase();
-    		word2 = word2.toLowerCase();
+    			word2 = scan.next();
+    			if(word2.equals("q"))
+    			{
+    				break;
+    			}
+    			word1 = word1.toLowerCase();
+    			word2 = word2.toLowerCase();
     		
-    		if (word1.length() != word2.length()) {            //length error
-    			System.out.println("The two words must be the same length.");
+    			if (word1.length() != word2.length()) {            //length error
+    				System.out.println("The two words must be the same length.");
+    			}
+    			else if ((!words.contains(word1)) || (!words.contains(word2))) {  //can't be found in the dictionary
+    				System.out.println("The two words must be found in the dictionary.");
+    			}
+    			else if (word1.equals(word2)) {    //same words
+    				System.out.println("The two words must be different.");
+    			}
+    			else {
+    				build_ladder(word1, word2, words);
+    				looptime =0;
+    				continue;
+    			}
+    			looptime++;
     		}
-    		else if ((!words.contains(word1)) || (!words.contains(word2))) {  //can't be found in the dictionary
-    			System.out.println("The two words must be found in the dictionary.");
-    		}
-    		else if (word1.equals(word2)) {    //same words
-    			System.out.println("The two words must be different.");
-    		}
-    		else {
-    			build_ladder(word1, word2, words);
-    		}
-    		looptime++;
+    		System.out.println("Have a nice day.");    //program ends
     	}
-    	System.out.println("Have a nice day.");    //program ends
-    }
     
     
     static File get_file() {
-    		String filename = "";
+    		String filename = new String();
     		System.out.println("Dictionary file name? ");
     		Scanner sc = new Scanner(System.in);   
     		filename = sc.nextLine();
     		
     		File file=new File(filename); 
     
-    		sc.close();
-    
     		if(!file.exists())    
-    		{    
+    		{   
     			return get_file();
     		}
     		else
+    		{
     			return file;
-    }	
-
+    
+    		}	
+    }
     static HashSet<String> read_words() {       //read the words in the dictionary
     
     		HashSet<String> words_set = new HashSet<String>();
     		
     		File file=get_file(); 
-    		
     		
    		Scanner scan = null;
 		try {
@@ -99,11 +107,10 @@ public class App
 
     		String word = "";
 	
-    		while (scan.hasNext()) {  
-        words_set.add(scan.next());
+    		while (scan.hasNext() ) { 
+    				String str = scan.next();
+    				words_set.add(str);
     		}  
-	
-    		scan.close();
     		return words_set;
     }		
 
@@ -114,7 +121,7 @@ public class App
     		Stack<String>word_ladder = new Stack<String>();
     		Stack<String>copy_wdladder = new Stack<String>();
     		String this_word = "", next_word = "";
-    		char change_letter = ' ';
+
     		word_ladder.push(word1);     //put the word into the ladder
     		used_words.add(word1);
     		ladders.add(word_ladder);
@@ -124,18 +131,17 @@ public class App
     			for (int i = 0; i < this_word.length(); i++) {
     				next_word = this_word;               
     				for (int j = 0; j < letter_num; j++) {
-    					next_word.charAt(i);
     					StringBuffer strbuffer=new StringBuffer(next_word);
     					strbuffer.setCharAt(i,(char)(next_word.charAt(i)+1)); //the letter in this positon is changed to the next letter (for example a turns to b) 
-    					if (next_word.charAt(i) >= 'z') {       
+    					if (next_word.charAt(i) > 'z') {       
     						strbuffer.setCharAt(i,(char)(next_word.charAt(i)-26)) ;    //control the character between a--z
     					}	
-				
+    					next_word = strbuffer.toString();
     					if (words.contains(next_word)) {       //check if the word is in the dictionary
     						if (!used_words.contains(next_word)) {   //to know if that word is used or not
     							word_ladder.push(next_word);
     							used_words.add(next_word);
-    							if (next_word == word2) {               //find word2 and the loop ends
+    							if (next_word.equals(word2)) {               //find word2 and the loop ends
     								System.out.println("A ladder from " + word2 + " back to " + word1 + ':' );
     								while (!word_ladder.empty()) {
     									System.out.print( word_ladder.pop() + ' ');    //output of wordladder
@@ -143,7 +149,7 @@ public class App
     								return;
     							}	
     							else {
-    								copy_wdladder = word_ladder;      //if next word is not word2,then put the ladder into the queue
+    								copy_wdladder = (Stack<String>) word_ladder.clone();      //if next word is not word2,then put the ladder into the queue
     								ladders.add(copy_wdladder);
     								word_ladder.pop();      //the first ladder out 
     							}
@@ -153,6 +159,7 @@ public class App
     			}
     		}
     		System.out.println("No word ladder found from " + word2 + " back to " + word1+'.');
+
     }	
 }
 
